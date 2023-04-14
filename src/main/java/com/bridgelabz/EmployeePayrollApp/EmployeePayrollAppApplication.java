dca27c559abfbc252514f31572815b132691aa21
unknown <gownideepu23@gmail.com>
2023-04-14 08:18:46 +0530

    added

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -1,13 +1,17 @@
 package com.bridgelabz.EmployeePayrollApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+//		System.out.println("Welcome to Employee payroll");
+		log.info("Application is started");
 	}
 }
