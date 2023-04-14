dc6471b90f5d6a075352ce134b71360db7ff8ff9
unknown <gownideepu23@gmail.com>
2023-04-14 08:42:45 +0530

    added

--- src/main/java/com/bridgelabz/EmployeePayrollApp/Dto/EmployeeDto.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/Dto/EmployeeDto.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.EmployeePayrollApp.Dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.List;
+
 @Data
 public class EmployeeDto {
+    @NotEmpty
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$",message = "The Name is notEmpty")
+
     private String name;
+
+    @Min(value = 400,message = "The minumum salary shoud be >400")
+
     private double salary;
+
 }
