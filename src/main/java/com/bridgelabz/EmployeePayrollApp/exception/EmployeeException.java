6d9cf51de3ac9220c9ae3c58f870ea64fdac5788
unknown <gownideepu23@gmail.com>
2023-04-15 08:45:33 +0530

    added

--- src/main/java/com/bridgelabz/EmployeePayrollApp/exception/EmployeeException.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/exception/EmployeeException.java
@@ -4,6 +4,7 @@ package com.bridgelabz.EmployeePayrollApp.exception;
 import com.bridgelabz.EmployeePayrollApp.Dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +22,11 @@ public class EmployeeException {
         ResponseDto responceDto=new ResponseDto("Exception While handling Rest Api call ,",errorMessage);
         return  new ResponseEntity<>(responceDto, HttpStatus.BAD_REQUEST);
 
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDto> HttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        ResponseDto responceDto=new ResponseDto("The Date shoid be in dd-mm-yyyy formate",exception.getMessage());
+        return new ResponseEntity<>(responceDto, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(EmployeeCustomException.class)
     public ResponseEntity<ResponseDto> handleEmployeeException(EmployeeCustomException exception){
